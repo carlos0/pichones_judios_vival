d7d8d364a98e98895d590b5d86144a8be2663c8d
Adrian <dbarra@agetic.gob.bo>
2017-11-15 13:45:44 -0400

    cambios

--- practica2/eje_2_13.sh
+++ practica2/eje_2_13.sh
@@ -6,12 +6,12 @@ read -p "Ingrese el segundo numero: " num2
 re='^[0-9]+$'
 if ! [[ $num1  =~ $re ]];
   then
-  echo "Error: El primer parametro no es numero" >&2; exit 1
+  echo "Error: El primer numero no es valido" >&2; exit 1
   exit 3
 fi
 if ! [[ $num2  =~ $re ]];
   then
-  echo "Error: El segundo parametro no es numero" >&2; exit 1
+  echo "Error: El segundo numero no es valido" >&2; exit 1
   exit 3
 fi
 
@@ -20,9 +20,16 @@ if [ ! -z $num1 ] && [ ! -z $num2 ];
     echo "Sumando ambos valores `expr $num1 + $num2`"
     echo "Restando ambos valores `expr $num1 - $num2`"
     echo "Multiplicando ambos valores `expr $num1 \* $num2`"
-    echo "Dividiendo ambos valores `expr $num1 / $num2`"
+    if [ $num2 -eq 0 ]
+     then
+       echo "No se puede dividir entre un numero 0"
+       exit 9
+     else
+      echo "Dividiendo ambos valores `expr $num1 / $num2`"
+   fi
   else
-    echo "Error al ingresar parametros"
+    echo "Uno de los numero esta vacio o los dos estan vacios"
     exit 1
 fi
 exit 0
+
